2ddf922ae7c1813adcf4126e0e31597e0f8c5270
Ting-Jui Tseng <albert.tseng@cardinalblue.com>
2023-06-15 03:23:50 +0800

    feat: take the photo and upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import camera from "./assets/camera.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setImageUrl(imageUrl);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="w-[512px] h-[512px] flex justify-center items-center bg-gray-300 rounded-md">
+        {imageUrl ? (
+          <img className="" src={imageUrl} alt="Image" />
+        ) : (
+          <label htmlFor="img">
+            <img src={camera} alt="camera" />
+            <input
+              className="hidden"
+              type="file"
+              id="img"
+              accept="image/*"
+              capture="environment"
+              onChange={(e) => handleOnChange(e)}
+            />
+          </label>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
