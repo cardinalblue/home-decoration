93d728df050604c5f3727fa3044bd2fcea263bec
Ting-Jui Tseng <albert.tseng@cardinalblue.com>
2023-06-15 23:11:27 +0800

    feat: update background.svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Header from "./components/Header";
 
 function App() {
+  const [step, setStep] = useState(0);
+  const [imgUrl, setImgUrl] = useState("");
+
   return (
     <div className="h-screen bg-[url('./assets/background.svg')]">
       <Header />
