33aa05bf3b8dc6b6f1606502664f3a94f90eec5d
Ting-Jui Tseng <albert.tseng@cardinalblue.com>
2023-06-16 16:36:58 +0800

    feat: tweak ui

--- src/pages/Stylings.tsx
+++ src/pages/Stylings.tsx
@@ -17,6 +17,7 @@ interface StylingsImgType {
 function Stylings({ imgUrl, originalImg }: StylingsProps) {
   const [stylingsImgs, setStylingsImgs] = useState<StylingsImgType[]>([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string>(imgUrl);
 
   const generateImagesWithStyles = async (imageFile: File) => {
     setIsFetching(true);
@@ -47,29 +48,53 @@ function Stylings({ imgUrl, originalImg }: StylingsProps) {
     <Loading />
   ) : (
     <div className="w-screen h-full flex flex-col gap-5 bg-[#E8E4E1]">
-      <div className="w-full px-5 flex justify-between text-white font-semibold">
-        <button className="w-[124px] h-[54px] uppercase bg-[#5B6B62] rounded-lg">
-          Back
-        </button>
-        <button className="w-[124px] h-[54px] uppercase bg-[#CC7541] rounded-lg">
-          Next
-        </button>
-      </div>
-      <div className="w-screen px-6 py-2 flex flex-row items-center justify-center gap-10">
+      <div className="w-screen grow px-6 py-2 flex flex-row items-center justify-around">
         <img
-          src={imgUrl}
-          alt="original"
-          className="w-[800px] h-[600px] object-contain"
+          src={selectedImage}
+          alt="selected"
+          className="w-[800px] h-[600px] object-contain rounded-md"
         />
-        <div className="w-[250px] flex flex-col">
-          <img src={imgUrl} alt="original" className="object-contain" />
-          {stylingsImgs.map((img) => (
+        <div className="w-[250px] h-full flex flex-col">
+          <div className="flex flex-col">
+            <div
+              className={[
+                "uppercase",
+                selectedImage === imgUrl ? " text-orange-500" : "",
+              ].join(" ")}
+            >
+              Original Photo
+            </div>
             <img
-              key={img.url}
-              src={img.url}
-              alt="styling"
-              className="object-contain"
+              src={imgUrl}
+              alt="original"
+              className={[
+                "w-[250px] h[200px] object-contain rounded-md",
+                selectedImage === imgUrl ? "ring-4 ring-orange-500" : "",
+              ].join(" ")}
+              onClick={() => setSelectedImage(imgUrl)}
             />
+          </div>
+          {stylingsImgs.map((img) => (
+            <div className="flex flex-col">
+              <div
+                className={[
+                  "uppercase",
+                  selectedImage === img.url ? " text-orange-500" : "",
+                ].join(" ")}
+              >
+                {img.style}
+              </div>
+              <img
+                key={img.url}
+                src={img.url}
+                alt="styling"
+                className={[
+                  "w-[250px] h[200px] object-contain rounded-md",
+                  selectedImage === img.url ? "ring-4 ring-orange-500" : "",
+                ].join(" ")}
+                onClick={() => setSelectedImage(img.url)}
+              />
+            </div>
           ))}
         </div>
       </div>
