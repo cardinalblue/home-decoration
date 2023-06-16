8abfedc88337054b92c3da5fac458b345cc36b51
Ting-Jui Tseng <albert.tseng@cardinalblue.com>
2023-06-16 14:57:26 +0800

    feat: remove unused var

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { useState } from "react";
 import UploadImage from "./pages/UploadImage";
-import Loading from "./pages/Loading";
 import Stylings from "./pages/Stylings";
 
 function App() {
   const [step, setStep] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
   const [originalImg, setOriginalImg] = useState<File | null>(null);
   const [imgUrl, setImgUrl] = useState<string | null>(null);
 
   return (
     <>
-      {isLoading && <Loading />}
       {step === 0 && (
         <UploadImage
           setStep={setStep}
